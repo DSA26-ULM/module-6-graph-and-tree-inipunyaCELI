37ba675ac83b87d10bc142fd85bd8f965a3554da
inipunyaCELI <543221158@smktelkom-bjb.sch.id>
2026-06-15 09:29:03 +0800

    soal1 ganti dikit

--- soal1.cpp
+++ soal1.cpp
@@ -1,30 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     int N;
     cin >> N;
 
-    vector<char> vertices(N);
+    vector<char> labels(N);
     map<char, int> idx;
-    for (int i = 0; i < N; i++)
-    {
-        cin >> vertices[i];
-        idx[vertices[i]] = i;
+    for (int i = 0; i < N; i++) {
+        cin >> labels[i];
+        idx[labels[i]] = i;
     }
 
     vector<vector<int>> matrix(N, vector<int>(N, 0));
 
     int M;
     cin >> M;
-    for (int i = 0; i < M; i++)
-    {
-        char u, v;
-        int w;
+    for (int i = 0; i < M; i++) {
+        char u, v; int w;
         cin >> u >> v >> w;
-        if (idx.count(u) && idx.count(v))
-        {
+        if (idx.count(u) && idx.count(v)) {
             matrix[idx[u]][idx[v]] = w;
         }
     }
@@ -33,12 +28,11 @@ int main()
 
     cout << " ";
     for (int i = 0; i < N; i++)
-        cout << " " << vertices[i];
+        cout << " " << labels[i];
     cout << endl;
 
-    for (int i = 0; i < N; i++)
-    {
-        cout << vertices[i];
+    for (int i = 0; i < N; i++) {
+        cout << labels[i];
         for (int j = 0; j < N; j++)
             cout << " " << matrix[i][j];
         cout << endl;
